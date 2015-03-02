d8e7658c6547547fadc1896e06ea0357bf9c36d2
Sun Daowen <sundaowen@gmail.com>
2015-03-02 23:18:23 +0800

    add Windows include and lib

---
+++ dep/src/libpng/_make_Windows_x86_64.bat
@@ -0,0 +1,36 @@
+IF DEFINED VS120COMNTOOLS (
+  SET GENERATOR="Visual Studio 12 Win64"
+) ELSE IF DEFINED VS110COMNTOOLS (
+  SET GENERATOR="Visual Studio 11 Win64"
+) ELSE IF DEFINED VS100COMNTOOLS (
+  SET GENERATOR="Visual Studio 10 Win64"
+)
+IF NOT DEFINED GENERATOR (
+  ECHO Can not find VC2010 or VC2012 or VC2013 installed!
+  GOTO ERROR
+)
+SET rootdir=%~dp0
+SET target=Windows_x86_64
+SET /P libpng_version=<"%rootdir%libpng.txt"
+RD /S /Q "%rootdir%%libpng_version%"
+MD "%rootdir%%libpng_version%"
+XCOPY "%rootdir%..\%libpng_version%" "%rootdir%%libpng_version%" /S /Y
+CD /D "%rootdir%%libpng_version%"
+MD project
+CD project
+cmake -C "%rootdir%CMakeLists.txt" -DCMAKE_INSTALL_PREFIX="%rootdir%%target%" -DZLIB_INCLUDE_DIR="%rootdir%..\..\include\%target%" -DZLIB_LIBRARY="%rootdir%..\..\lib\%target%\zlibstatic.lib" -G %GENERATOR% ..
+cmake -C "%rootdir%CMakeLists.txt" -DCMAKE_INSTALL_PREFIX="%rootdir%%target%" -DZLIB_INCLUDE_DIR="%rootdir%..\..\include\%target%" -DZLIB_LIBRARY="%rootdir%..\..\lib\%target%\zlibstatic.lib" -G %GENERATOR% ..
+cmake --build . --target install --config Release --clean-first
+RD /S /Q "%rootdir%%target%\include\libpng16"
+MD "%rootdir%..\..\include\%target%"
+XCOPY "%rootdir%%target%\include" "%rootdir%..\..\include\%target%" /S /Y
+MD "%rootdir%..\..\lib\%target%"
+COPY /Y "%rootdir%%target%\lib\*.lib" "%rootdir%..\..\lib\%target%"
+CD /D "%rootdir%"
+RD /S /Q "%rootdir%%libpng_version%"
+GOTO EOF
+
+:ERROR
+PAUSE
+
+:EOF
---
+++ dep/src/libpng/_make_linux_x86_32.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+rootdir=`dirname $0`
+cd $rootdir
+rootdir=`pwd`
+target=Linux_x86_32
+libpng_version=`cat $rootdir/libpng.txt`
+rm -rf "$rootdir/$libpng_version"
+mkdir "$rootdir/$libpng_version"
+cp -rf "$rootdir/../$libpng_version/"* "$rootdir/$libpng_version"
+cd "$rootdir/$libpng_version"
+mkdir project
+cd project
+cmake -DBUILD64=OFF -C "$rootdir/CMakeLists.txt" -DCMAKE_INSTALL_PREFIX="$rootdir/$target" -DZLIB_INCLUDE_DIR="$rootdir/../../include/$target" -DZLIB_LIBRARY="$rootdir/../../lib/$target/libz.a" ..
+cmake -DBUILD64=OFF -C "$rootdir/CMakeLists.txt" -DCMAKE_INSTALL_PREFIX="$rootdir/$target" -DZLIB_INCLUDE_DIR="$rootdir/../../include/$target" -DZLIB_LIBRARY="$rootdir/../../lib/$target/libz.a" ..
+cmake --build . --target install --config Release --clean-first
+rm -rf "$rootdir/$target/include/libpng16"
+mkdir "$rootdir/../../include/$target"
+cp -rf "$rootdir/$target/include/"* "$rootdir/../../include/$target"
+mkdir "$rootdir/../../lib/$target"
+cp -rf "$rootdir/$target/lib/"* "$rootdir/../../lib/$target"
+cd $rootdir
+rm -rf "$rootdir/$libpng_version"
