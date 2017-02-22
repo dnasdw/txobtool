1376e5bbb2eb119a4c37bdfcf553bf0182fae76b
Sun Daowen <sundaowen@gmail.com>
2017-02-22 17:16:32 +0800

    add shell script

---
+++ bin/txobtool.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cwdir=`pwd`
+rootdir=`dirname "$0"`
+cd "$rootdir"
+rootdir=`pwd`
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"$rootdir"
+cd "$cwdir"
+"$rootdir/txobtool_d" "$1" "$2" "$3" "$4" "$5" "$6"
